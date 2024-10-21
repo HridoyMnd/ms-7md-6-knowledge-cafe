45ff9c30f31f773d94e03754a5f2e307b5a45162
Hridoy Mnd <hridoymnd10@gmail.com>
2024-10-21 21:17:26 +0600

    react basic structure,create fake data,conect with tailwind,show fake data

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Blogs from './my-components/Blogs/Blogs'
+import Header from './my-components/header/Header'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/my-components/header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-blue-400 font-extrabold'>React Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
