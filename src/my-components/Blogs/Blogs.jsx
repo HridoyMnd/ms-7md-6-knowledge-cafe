a2704d3cea7e36c5d6286dfa723fb7efa7ba7f09
Hridoy Mnd <hridoymnd10@gmail.com>
2024-10-24 14:47:28 +0600

    all requrement in blogs.js done

--- src/my-components/Blogs/Blogs.jsx
+++ src/my-components/Blogs/Blogs.jsx
@@ -3,11 +3,11 @@
 const Blogs = ({ blogs }) => {
     console.log(blogs);
     return (
-       <div className="col-span-2">
+       <div className="col-span-2" key={blogs.author_id}>
             {
-                blogs.map((blog) => <div className="p-5 border rounded-lg border-red-400">
+                blogs.map((blog, id) =><div className="p-5 border rounded-lg border-red-400">
                     <img src={blog.cover_img} alt="" className="rounded-lg" />
-                    <div className="mt-4 flex justify-between items-center">
+                    <div className="my-5 flex justify-between items-center">
                         <div className="flex items-center gap-4">
                         <img src={blog.author_img} className="w-16" alt="" />
                         <div className="info">
@@ -15,7 +15,19 @@ const Blogs = ({ blogs }) => {
                             <h3>{blog.posted_date}</h3>
                         </div>
                         </div>
-                        <div className="time">{blog.reading_time}</div>
+                        <div className="time">{blog.reading_time}
+                            <button className="border border-blue-400 px-3 rounded-sm ml-2">click</button>
+                        </div>
+                    </div>
+                    <div className="">
+                        <h1 className="text-2xl font-bold">{blog.title}</h1>
+                        <div className="flex gap-3 my-2" key={id}>
+                         
+                        { 
+                            blog.hashtag.map((tag, idx) =><p key={idx}>{tag}</p>)
+                        }
+                        </div>
+                        <a href="" className="text-violet-800">mark as read</a>
                     </div>
                 </div>)
             }
